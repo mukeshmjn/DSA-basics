0b8605711f12bc9c2f985d1f88cf2e204e3de7d6
mukeshmjn <68895120+mukeshmjn@users.noreply.github.com>
2021-05-13 20:31:05 +0530

    upto queue using stack

---
+++ stacks/postfixEvaluation.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int prefixEvaluation(string s){
+
+    stack<int> st;
+    for(int i=0;i<s.length();i++){
+        if(s[i]>='0'&&s[i]<='9'){
+            st.push(s[i]-'0');
+        }
+        else{
+            int op2 = st.top();
+            st.pop();
+            int op1 = st.top();
+            st.pop();
+            switch (s[i])
+            {
+            case '+':
+            st.push(op1+op2);
+                break;
+                 case '-':
+            st.push(op1-op2);
+                break;
+                 case '*':
+            st.push(op1*op2);
+                break;
+                 case '/':
+            st.push(op1/op2);
+                break;
+
+        }
+    }
+}
+return st.top();
+}
+int main(){
+    cout<<prefixEvaluation("231*+9-");
+    return 0;
+}
